373aa8bbd9ef0d2e20e52e71831b547c89ad90c8
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2024-10-24 19:05:49 +0200

    fix: add validation pipe

---
+++ src/validation.pipe.ts
@@ -0,0 +1,29 @@
+import {
+  PipeTransform,
+  Injectable,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+import { validate } from 'class-validator';
+import { plainToInstance } from 'class-transformer';
+
+@Injectable()
+export class ValidationPipe implements PipeTransform<unknown> {
+  async transform(value: unknown, { metatype }: ArgumentMetadata) {
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
+    }
+    const object = plainToInstance(metatype, value);
+    const errors = await validate(object);
+    if (errors.length > 0) {
+      console.error(errors);
+      throw new BadRequestException('Bad Request');
+    }
+    return value;
+  }
+
+  private toValidate(metatype: Function): boolean {
+    const types: Function[] = [String, Boolean, Number, Array, Object];
+    return !types.includes(metatype);
+  }
+}

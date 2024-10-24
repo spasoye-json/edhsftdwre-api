3554d65bf9301ddb7ca2c175e2c2f5a8e11b2a08
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2024-10-24 19:54:26 +0200

    chore: add schedule module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { VerificationModule } from './verification/verification.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URL),
     VerificationModule,

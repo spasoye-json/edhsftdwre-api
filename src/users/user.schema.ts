d940c47e34428ef9f184f0e4dc3e5259acc28c43
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2024-10-26 23:52:02 +0200

    feat(user): add preferences to schema

--- src/users/user.schema.ts
+++ src/users/user.schema.ts
@@ -1,6 +1,10 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
+type UserPreferences = {
+  emailNotifications: boolean;
+};
+
 @Schema({ collection: 'users', timestamps: true })
 export class User extends Document {
   @Prop({ required: true, unique: true })
@@ -8,6 +12,16 @@ export class User extends Document {
 
   @Prop({ required: true })
   password: string;
+
+  @Prop({
+    type: Object,
+    raw: {
+      emailNotifications: { type: Boolean },
+    },
+    required: true,
+    default: { emailNotifications: true },
+  })
+  preferences: UserPreferences;
 
   @Prop({ required: true, default: false })
   verified: boolean;

ac05242b3b8a535eeb0ba6d8fc82d7d5d55a9213
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2024-10-26 23:52:15 +0200

    feat(notifications): send email notifications

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -3,17 +3,34 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Notification } from './notification.schema';
 import { NotificationsGateway } from './notifications.gateway';
+import { UsersService } from 'src/users/users.service';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class NotificationsService {
   constructor(
     @InjectModel(Notification.name)
-    private notificationModel: Model<Notification>,
-    private notificationsGateway: NotificationsGateway,
+    private readonly notificationModel: Model<Notification>,
+    private readonly usersService: UsersService,
+    private readonly notificationsGateway: NotificationsGateway,
+    private readonly mailerService: MailerService,
   ) {}
 
   async sendNotification(userId: string, message: string) {
     const notification = new this.notificationModel({ userId, message });
+
+    const user = await this.usersService.findNotifiableUserById(userId);
+
+    if (user) {
+      await this.mailerService.sendMail({
+        to: user.email,
+        subject: 'New Notification',
+        template: './notification',
+        context: {
+          message,
+        },
+      });
+    }
 
     return await notification.save();
   }

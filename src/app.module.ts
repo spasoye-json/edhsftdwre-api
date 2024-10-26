6c4913a63be4cba0a2d45251411110083822d350
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2024-10-26 23:36:48 +0200

    feat(mail): setup mailer module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,27 @@ import { ConfigModule } from '@nestjs/config';
 import { VerificationModule } from './verification/verification.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { JobQueueModule } from './job-queue/job-queue.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URL),
+    MailerModule.forRoot({
+      transport: process.env.SMTP_URL,
+      defaults: {
+        from: "'No Reply' <noreply@edhsftdwre-api.com>",
+      },
+      template: {
+        dir: __dirname + '/../templates',
+        adapter: new EjsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
     VerificationModule,
     UsersModule,
     NotificationsModule,

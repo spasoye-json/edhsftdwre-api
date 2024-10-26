211389fe2b60948de51a1d18462946a0c70b4820
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2024-10-26 23:14:58 +0200

    feat(notification): add notification created event

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Post, Body, Param, Get, UseGuards } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { JobQueueService } from 'src/job-queue/job-queue.service';
+import { createNotificationCreatedEvent } from './notification.mq';
 
 @Controller('notifications')
 export class NotificationsController {
-  constructor(private readonly notificationsService: NotificationsService) {}
+  constructor(
+    private readonly notificationsService: NotificationsService,
+    private readonly jobQueueService: JobQueueService,
+  ) {}
 
   @UseGuards(JwtAuthGuard)
   @Get(':userId/notifications')
@@ -18,6 +23,15 @@ export class NotificationsController {
     @Param('userId') userId: string,
     @Body('message') message: string,
   ) {
-    return this.notificationsService.sendNotification(userId, message);
+    const notification = await this.notificationsService.sendNotification(
+      userId,
+      message,
+    );
+
+    this.jobQueueService.sendTaskMessage(
+      createNotificationCreatedEvent(notification),
+    );
+
+    return notification;
   }
 }

68e8e17222406793f75b8136a10a608c326e0d86
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2024-10-26 23:41:15 +0200

    chore(mongodb): add timestamp fields

--- src/users/user.schema.ts
+++ src/users/user.schema.ts
@@ -1,16 +1,13 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-@Schema({ collection: 'users' })
+@Schema({ collection: 'users', timestamps: true })
 export class User extends Document {
   @Prop({ required: true, unique: true })
   email: string;
 
   @Prop({ required: true })
   password: string;
-
-  @Prop({ required: true, default: new Date() })
-  createdAt: Date;
 
   @Prop({ required: true, default: false })
   verified: boolean;
--- src/verification/verification.schema.ts
+++ src/verification/verification.schema.ts
@@ -2,16 +2,13 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { Document } from 'mongoose';
 import { User } from 'src/users/user.schema';
 
-@Schema({ collection: 'verification-codes' })
+@Schema({ collection: 'verification-codes', timestamps: true })
 export class VerificationCode extends Document {
   @Prop({ required: true, unique: true })
   code: string;
 
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
   user: User;
-
-  @Prop({ required: true, default: new Date() })
-  createdAt: Date;
 }
 
 export const VerificationCodeSchema =

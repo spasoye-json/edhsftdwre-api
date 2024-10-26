d7954855318804d715141f4af6fe9611cca85699
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2024-10-27 00:05:19 +0200

    feat(notifications): use mq to handle task

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -5,14 +5,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Notification, NotificationSchema } from './notification.schema';
 import { NotificationsController } from './notifications.controller';
 import { JobQueueModule } from 'src/job-queue/job-queue.module';
-import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Notification.name, schema: NotificationSchema },
     ]),
-    UsersModule,
     JobQueueModule,
   ],
   providers: [NotificationsGateway, NotificationsService],

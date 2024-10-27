3449a83c5820050a7f947e6743a003fa2065d764
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2024-10-27 14:24:47 +0100

    feat(misc): add fibonacci sequence

--- src/misc/misc.controller.ts
+++ src/misc/misc.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { FizzBuzzService } from './fizzbuzz.service';
+import { FibonacciService } from './fibonacci.service';
 
 @Controller('misc')
 export class MiscController {
-  constructor(private readonly fizzBuzzService: FizzBuzzService) {}
+  constructor(
+    private readonly fizzBuzzService: FizzBuzzService,
+    private readonly fibonacciService: FibonacciService,
+  ) {}
 
   @Get('fizzbuzz/:number')
   async getFizzBuzz(@Param('number') number: string) {
     return this.fizzBuzzService.getFizzBuzz(number);
+  }
+
+  @Get('fibonacci/:number')
+  async getFibonacci(@Param('number') number: string) {
+    return this.fibonacciService.getFibonacci(number);
   }
 }

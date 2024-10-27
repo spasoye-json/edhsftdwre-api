eaaecf98cddae7e84acf066025accda5159094d9
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2024-10-27 14:03:49 +0100

    feat(misc): implement FizzBuzz endpoint & service

--- src/misc/misc.controller.ts
+++ src/misc/misc.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { MiscService } from './misc.service';
 
 @Controller('misc')
-export class MiscController {}
+export class MiscController {
+  constructor(private readonly miscService: MiscService) {}
+
+  @Get('fizzbuzz/:number')
+  async getFizzBuzz(@Param('number') number: string) {
+    return this.miscService.getFizzBuzz(number);
+  }
+}
--- src/misc/misc.service.ts
+++ src/misc/misc.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { z } from 'zod';
 
 @Injectable()
-export class MiscService {}
+export class MiscService {
+  async getFizzBuzz(number: string) {
+    const result = z.coerce.number().safeParse(number);
+
+    if (!result.success) {
+      throw new BadRequestException('Invalid number');
+    }
+
+    const { data: value } = result;
+
+    let response = '';
+
+    if (value % 3 === 0) {
+      response += 'Fizz';
+    }
+    if (value % 5 === 0) {
+      response += 'Buzz';
+    }
+
+    return {
+      result: response || value.toString(),
+    };
+  }
+}

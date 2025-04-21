def338b134fb0de83af0ff11d80bdb2cbdcc410f
Zvonimir Rudinski <zvonki12@gmail.com>
2025-04-21 09:41:12 +0200

    feat(misc): add echo service to providers

--- src/misc/misc.module.ts
+++ src/misc/misc.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MiscController } from './misc.controller';
 import { FizzBuzzService } from './fizzbuzz.service';
 import { FibonacciService } from './fibonacci.service';
+import { EchoService } from './echo.service';
 
 @Module({
   controllers: [MiscController],
-  providers: [FizzBuzzService, FibonacciService],
+  providers: [FizzBuzzService, FibonacciService, EchoService],
 })
 export class MiscModule {}

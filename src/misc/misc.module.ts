17f76776d481e2502dad14a114408e2e50825bee
Zvonimir Rudinski <zvonki12@gmail.com>
2025-04-21 09:55:27 +0200

    feat(misc): add IsEvenService and IsOddService to misc module providers

--- src/misc/misc.module.ts
+++ src/misc/misc.module.ts
@@ -3,9 +3,17 @@ import { MiscController } from './misc.controller';
 import { FizzBuzzService } from './fizzbuzz.service';
 import { FibonacciService } from './fibonacci.service';
 import { EchoService } from './echo.service';
+import { IsEvenService } from './is-even.service';
+import { IsOddService } from './is-odd.service';
 
 @Module({
   controllers: [MiscController],
-  providers: [FizzBuzzService, FibonacciService, EchoService],
+  providers: [
+    FizzBuzzService,
+    FibonacciService,
+    EchoService,
+    IsEvenService,
+    IsOddService,
+  ],
 })
 export class MiscModule {}

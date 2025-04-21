156fb34ce597633d75e44c6b10e16aa42996536b
Zvonimir Rudinski <zvonki12@gmail.com>
2025-04-21 09:41:00 +0200

    feat(echo): add echo endpoint to misc controller

--- src/misc/misc.controller.ts
+++ src/misc/misc.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { FizzBuzzService } from './fizzbuzz.service';
 import { FibonacciService } from './fibonacci.service';
+import { EchoService } from './echo.service';
 
 @Controller('misc')
 export class MiscController {
   constructor(
     private readonly fizzBuzzService: FizzBuzzService,
     private readonly fibonacciService: FibonacciService,
+    private readonly echoService: EchoService,
   ) {}
 
   @Get('fizzbuzz/:number')
@@ -17,5 +19,10 @@ export class MiscController {
   @Get('fibonacci/:number')
   async getFibonacci(@Param('number') number: string) {
     return this.fibonacciService.getFibonacci(number);
+  }
+
+  @Get('echo/:message')
+  async getEcho(@Param('message') message: string) {
+    return this.echoService.getEcho(message);
   }
 }

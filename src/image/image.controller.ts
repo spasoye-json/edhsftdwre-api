15ffd6e3cb2ec33eb5d8008fdfc3673dcf7c8601
Zvonimir Rudinski <zvonki12@gmail.com>
2025-04-21 10:36:35 +0200

    feat(image): allow specifying size of random image

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
 import { ImageService } from './image.service';
 import { Response } from 'express';
 
@@ -8,7 +8,30 @@ export class ImageController {
 
   @Get('random')
   async getRandomImage(@Res() response: Response) {
-    const url = await this.imageService.getRandomImageUrl();
+    const url = await this.imageService.getRandomImageUrl(
+      this.imageService.getDefaultImageSize(),
+    );
+    response.redirect(url);
+  }
+
+  @Get('random/:size')
+  async getRandomImageWithSize(
+    @Res() response: Response,
+    @Param('size') size: string,
+  ) {
+    const imageSize = this.imageService.parseImageSize(size, size);
+    const url = await this.imageService.getRandomImageUrl(imageSize);
+    response.redirect(url);
+  }
+
+  @Get('random/:width/:height')
+  async getRandomImageWithWidthAndHeight(
+    @Res() response: Response,
+    @Param('width') width: string,
+    @Param('height') height: string,
+  ) {
+    const imageSize = this.imageService.parseImageSize(width, height);
+    const url = await this.imageService.getRandomImageUrl(imageSize);
     response.redirect(url);
   }
 }
--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,19 +1,51 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+import { z } from 'zod';
 
-// TODO: Make this a configurable value
-const DEFAULT_SIZE = 200;
+type ImageSize = {
+  width: number;
+  height: number;
+};
 
 @Injectable()
 export class ImageService {
   private readonly logger = new Logger(ImageService.name);
+  private readonly imageSizeSchema = z.object({
+    width: z.coerce.number().int().gt(0),
+    height: z.coerce.number().int().gt(0),
+  });
 
-  async getRandomImageUrl() {
-    this.logger.log('Fetching random image URL');
-    return this.generateRandomImageUrl(DEFAULT_SIZE);
+  getDefaultImageSize(): ImageSize {
+    return { width: 640, height: 480 };
   }
 
-  private generateRandomImageUrl(size: number) {
+  parseImageSize(width: string, height: string): ImageSize {
+    const result = this.imageSizeSchema.safeParse({
+      width,
+      height,
+    });
+
+    if (!result.success) {
+      this.logger.log('Invalid image size');
+
+      throw new BadRequestException('Invalid image size');
+    }
+
+    const w = result.data.width;
+    const h = result.data.height;
+
+    return {
+      width: w,
+      height: h,
+    };
+  }
+
+  async getRandomImageUrl(size: ImageSize) {
+    this.logger.log('Fetching random image URL');
+    return this.generateRandomImageUrl(size);
+  }
+
+  private generateRandomImageUrl(size: ImageSize) {
     this.logger.log(`Generating random image URL with size: ${size}`);
-    return `https://picsum.photos/${size}`;
+    return `https://picsum.photos/${size.width}/${size.height}`;
   }
 }
